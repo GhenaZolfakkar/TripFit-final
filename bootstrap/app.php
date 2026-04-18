34b89066a54d34fc4f279e456fbbe091c3ca3c42
Nagham Salah <naghamsalah607@gmail.com>
2026-04-18 15:49:48 +0200

    Edit For Deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ->withMiddleware(function ($middleware) {
+    $middleware->trustProxies(at: '*');
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
